c5877005b73de99de242c0dcac4a6b25e8197a08
Matheus Dias <123600746+MatheusDClima@users.noreply.github.com>
2023-09-28 12:04:33 -0300

    Novas atualizações

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var MenuItens = document.getElementById("MenuItens");
+
+MenuItens.style.maxHeight = "0px"; /* 0 significa q vai ser escondida */ 
+
+function menucelular(){
+    if(MenuItens.style.maxHeight == "0px"){
+    MenuItens.style.maxHeight = "200px";
+    }else{ 
+        MenuItens.style.maxHeight = "0px";
+    }
+}
